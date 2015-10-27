408d18cd8a32325fb96074aa71d5f7a42f03673a
Wordyj <lachlanword@gmail.com>
2015-10-27 18:39:35 +1100

    Quiz 1 Restart
    Starting to re-make quiz 1 in a more simple way and incorperating the
    new text

--- BrainLoad/Demo Quizes.Designer.vb
+++ BrainLoad/Demo Quizes.Designer.vb
@@ -24,12 +24,12 @@ Partial Class Demo_Quizes
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.NewYork = New System.Windows.Forms.RadioButton()
-        Me.Sydney = New System.Windows.Forms.RadioButton()
-        Me.Chicago = New System.Windows.Forms.RadioButton()
-        Me.QuizNext1 = New System.Windows.Forms.Button()
-        Me.qu1q = New System.Windows.Forms.Panel()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Answer1a = New System.Windows.Forms.RadioButton()
+        Me.Answer1c = New System.Windows.Forms.RadioButton()
+        Me.Answer1b = New System.Windows.Forms.RadioButton()
+        Me.Question1Submit = New System.Windows.Forms.Button()
+        Me.Question1 = New System.Windows.Forms.Panel()
+        Me.Answer1d = New System.Windows.Forms.RadioButton()
         Me.qu1q3 = New System.Windows.Forms.Panel()
         Me.Turcor = New System.Windows.Forms.RadioButton()
         Me.QuizNext3 = New System.Windows.Forms.Button()
@@ -51,23 +51,20 @@ Partial Class Demo_Quizes
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.F = New System.Windows.Forms.RadioButton()
-        Me.qu1q5 = New System.Windows.Forms.Panel()
-        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
-        Me.QuizNext5 = New System.Windows.Forms.Button()
+        Me.Question2 = New System.Windows.Forms.Panel()
+        Me.Question2Text = New System.Windows.Forms.TextBox()
+        Me.Question2Submit = New System.Windows.Forms.Button()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.CapBa = New System.Windows.Forms.RadioButton()
-        Me.CapB = New System.Windows.Forms.RadioButton()
-        Me.RadioButton9 = New System.Windows.Forms.RadioButton()
         Me.QuizNext4 = New System.Windows.Forms.Button()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.CapA = New System.Windows.Forms.RadioButton()
         Me.RadioButton11 = New System.Windows.Forms.RadioButton()
         Me.RadioButton12 = New System.Windows.Forms.RadioButton()
-        Me.qu1q.SuspendLayout()
+        Me.Question1.SuspendLayout()
         Me.qu1q3.SuspendLayout()
         Me.qu1q2.SuspendLayout()
         Me.Qu1q4.SuspendLayout()
-        Me.qu1q5.SuspendLayout()
+        Me.Question2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -84,77 +81,77 @@ Partial Class Demo_Quizes
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(3, 28)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(130, 13)
+        Me.Label2.Size = New System.Drawing.Size(202, 13)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "What is a green product ?"
+        Me.Label2.Text = "What is the age of Phillip and his mother?"
         '
-        'NewYork
+        'Answer1a
         '
-        Me.NewYork.AutoSize = True
-        Me.NewYork.Location = New System.Drawing.Point(3, 55)
-        Me.NewYork.Name = "NewYork"
-        Me.NewYork.Size = New System.Drawing.Size(94, 17)
-        Me.NewYork.TabIndex = 2
-        Me.NewYork.TabStop = True
-        Me.NewYork.Text = "One that is fun"
-        Me.NewYork.UseVisualStyleBackColor = True
+        Me.Answer1a.AutoSize = True
+        Me.Answer1a.Location = New System.Drawing.Point(3, 55)
+        Me.Answer1a.Name = "Answer1a"
+        Me.Answer1a.Size = New System.Drawing.Size(79, 17)
+        Me.Answer1a.TabIndex = 2
+        Me.Answer1a.TabStop = True
+        Me.Answer1a.Text = "a) 1 and 40"
+        Me.Answer1a.UseVisualStyleBackColor = True
         '
-        'Sydney
+        'Answer1c
         '
-        Me.Sydney.AutoSize = True
-        Me.Sydney.Location = New System.Drawing.Point(3, 101)
-        Me.Sydney.Name = "Sydney"
-        Me.Sydney.Size = New System.Drawing.Size(151, 17)
-        Me.Sydney.TabIndex = 3
-        Me.Sydney.TabStop = True
-        Me.Sydney.Text = "One that is mixed together."
-        Me.Sydney.UseVisualStyleBackColor = True
+        Me.Answer1c.AutoSize = True
+        Me.Answer1c.Location = New System.Drawing.Point(3, 101)
+        Me.Answer1c.Name = "Answer1c"
+        Me.Answer1c.Size = New System.Drawing.Size(79, 17)
+        Me.Answer1c.TabIndex = 3
+        Me.Answer1c.TabStop = True
+        Me.Answer1c.Text = "c) 5 and 40"
+        Me.Answer1c.UseVisualStyleBackColor = True
         '
-        'Chicago
+        'Answer1b
         '
-        Me.Chicago.AccessibleName = ""
-        Me.Chicago.AutoSize = True
-        Me.Chicago.Location = New System.Drawing.Point(3, 78)
-        Me.Chicago.Name = "Chicago"
-        Me.Chicago.Size = New System.Drawing.Size(162, 17)
-        Me.Chicago.TabIndex = 4
-        Me.Chicago.TabStop = True
-        Me.Chicago.Text = "One that is safe for the earth."
-        Me.Chicago.UseVisualStyleBackColor = True
+        Me.Answer1b.AccessibleName = ""
+        Me.Answer1b.AutoSize = True
+        Me.Answer1b.Location = New System.Drawing.Point(3, 78)
+        Me.Answer1b.Name = "Answer1b"
+        Me.Answer1b.Size = New System.Drawing.Size(79, 17)
+        Me.Answer1b.TabIndex = 4
+        Me.Answer1b.TabStop = True
+        Me.Answer1b.Text = "b) 6 and 26" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Answer1b.UseVisualStyleBackColor = True
         '
-        'QuizNext1
+        'Question1Submit
         '
-        Me.QuizNext1.Location = New System.Drawing.Point(3, 152)
-        Me.QuizNext1.Name = "QuizNext1"
-        Me.QuizNext1.Size = New System.Drawing.Size(75, 23)
-        Me.QuizNext1.TabIndex = 5
-        Me.QuizNext1.Text = "Next"
-        Me.QuizNext1.UseVisualStyleBackColor = True
+        Me.Question1Submit.Location = New System.Drawing.Point(3, 152)
+        Me.Question1Submit.Name = "Question1Submit"
+        Me.Question1Submit.Size = New System.Drawing.Size(75, 23)
+        Me.Question1Submit.TabIndex = 5
+        Me.Question1Submit.Text = "Next"
+        Me.Question1Submit.UseVisualStyleBackColor = True
         '
-        'qu1q
+        'Question1
         '
-        Me.qu1q.Controls.Add(Me.RadioButton3)
-        Me.qu1q.Controls.Add(Me.Label1)
-        Me.qu1q.Controls.Add(Me.QuizNext1)
-        Me.qu1q.Controls.Add(Me.Label2)
-        Me.qu1q.Controls.Add(Me.Sydney)
-        Me.qu1q.Controls.Add(Me.Chicago)
-        Me.qu1q.Controls.Add(Me.NewYork)
-        Me.qu1q.Location = New System.Drawing.Point(12, 3)
-        Me.qu1q.Name = "qu1q"
-        Me.qu1q.Size = New System.Drawing.Size(279, 259)
-        Me.qu1q.TabIndex = 6
+        Me.Question1.Controls.Add(Me.Answer1d)
+        Me.Question1.Controls.Add(Me.Label1)
+        Me.Question1.Controls.Add(Me.Question1Submit)
+        Me.Question1.Controls.Add(Me.Label2)
+        Me.Question1.Controls.Add(Me.Answer1c)
+        Me.Question1.Controls.Add(Me.Answer1b)
+        Me.Question1.Controls.Add(Me.Answer1a)
+        Me.Question1.Location = New System.Drawing.Point(12, 3)
+        Me.Question1.Name = "Question1"
+        Me.Question1.Size = New System.Drawing.Size(279, 259)
+        Me.Question1.TabIndex = 6
         '
-        'RadioButton3
+        'Answer1d
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(3, 124)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(156, 17)
-        Me.RadioButton3.TabIndex = 6
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "One that is made from grass"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.Answer1d.AutoSize = True
+        Me.Answer1d.Location = New System.Drawing.Point(3, 124)
+        Me.Answer1d.Name = "Answer1d"
+        Me.Answer1d.Size = New System.Drawing.Size(79, 17)
+        Me.Answer1d.TabIndex = 6
+        Me.Answer1d.TabStop = True
+        Me.Answer1d.Text = "d) 5 and 33"
+        Me.Answer1d.UseVisualStyleBackColor = True
         '
         'qu1q3
         '
@@ -174,14 +171,15 @@ Partial Class Demo_Quizes
         Me.Turcor.AutoSize = True
         Me.Turcor.Location = New System.Drawing.Point(3, 124)
         Me.Turcor.Name = "Turcor"
-        Me.Turcor.Size = New System.Drawing.Size(146, 17)
+        Me.Turcor.Size = New System.Drawing.Size(38, 17)
         Me.Turcor.TabIndex = 6
         Me.Turcor.TabStop = True
-        Me.Turcor.Text = "Different Shades of green"
+        Me.Turcor.Text = "Bill"
         Me.Turcor.UseVisualStyleBackColor = True
         '
         'QuizNext3
         '
+        Me.QuizNext3.Enabled = False
         Me.QuizNext3.Location = New System.Drawing.Point(3, 152)
         Me.QuizNext3.Name = "QuizNext3"
         Me.QuizNext3.Size = New System.Drawing.Size(75, 23)
@@ -194,19 +192,19 @@ Partial Class Demo_Quizes
         Me.Label4.AutoSize = True
         Me.Label4.Location = New System.Drawing.Point(3, 28)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.Size = New System.Drawing.Size(168, 13)
         Me.Label4.TabIndex = 1
-        Me.Label4.Text = "Turtle are..." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Label4.Text = "What is the name of the snowman" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'ninteenoate
         '
         Me.ninteenoate.AutoSize = True
         Me.ninteenoate.Location = New System.Drawing.Point(3, 101)
         Me.ninteenoate.Name = "ninteenoate"
-        Me.ninteenoate.Size = New System.Drawing.Size(116, 17)
+        Me.ninteenoate.Size = New System.Drawing.Size(57, 17)
         Me.ninteenoate.TabIndex = 3
         Me.ninteenoate.TabStop = True
-        Me.ninteenoate.Text = "Not found in nature"
+        Me.ninteenoate.Text = "Snowy"
         Me.ninteenoate.UseVisualStyleBackColor = True
         '
         'nineteensixty
@@ -215,10 +213,10 @@ Partial Class Demo_Quizes
         Me.nineteensixty.AutoSize = True
         Me.nineteensixty.Location = New System.Drawing.Point(3, 78)
         Me.nineteensixty.Name = "nineteensixty"
-        Me.nineteensixty.Size = New System.Drawing.Size(99, 17)
+        Me.nineteensixty.Size = New System.Drawing.Size(44, 17)
         Me.nineteensixty.TabIndex = 4
         Me.nineteensixty.TabStop = True
-        Me.nineteensixty.Text = "Green Products"
+        Me.nineteensixty.Text = "Bob"
         Me.nineteensixty.UseVisualStyleBackColor = True
         '
         'nineteenhundred
@@ -226,10 +224,10 @@ Partial Class Demo_Quizes
         Me.nineteenhundred.AutoSize = True
         Me.nineteenhundred.Location = New System.Drawing.Point(3, 55)
         Me.nineteenhundred.Name = "nineteenhundred"
-        Me.nineteenhundred.Size = New System.Drawing.Size(74, 17)
+        Me.nineteenhundred.Size = New System.Drawing.Size(43, 17)
         Me.nineteenhundred.TabIndex = 2
         Me.nineteenhundred.TabStop = True
-        Me.nineteenhundred.Text = "Not Green"
+        Me.nineteenhundred.Text = "Billy"
         Me.nineteenhundred.UseVisualStyleBackColor = True
         '
         'qu1q2
@@ -384,81 +382,40 @@ Partial Class Demo_Quizes
         Me.F.Text = "a secondary color"
         Me.F.UseVisualStyleBackColor = True
         '
-        'qu1q5
+        'Question2
         '
-        Me.qu1q5.Controls.Add(Me.RadioButton4)
-        Me.qu1q5.Controls.Add(Me.QuizNext5)
-        Me.qu1q5.Controls.Add(Me.Label8)
-        Me.qu1q5.Controls.Add(Me.CapBa)
-        Me.qu1q5.Controls.Add(Me.CapB)
-        Me.qu1q5.Controls.Add(Me.RadioButton9)
-        Me.qu1q5.Location = New System.Drawing.Point(12, 298)
-        Me.qu1q5.Name = "qu1q5"
-        Me.qu1q5.Size = New System.Drawing.Size(279, 259)
-        Me.qu1q5.TabIndex = 8
+        Me.Question2.Controls.Add(Me.Question2Text)
+        Me.Question2.Controls.Add(Me.Question2Submit)
+        Me.Question2.Controls.Add(Me.Label8)
+        Me.Question2.Location = New System.Drawing.Point(12, 298)
+        Me.Question2.Name = "Question2"
+        Me.Question2.Size = New System.Drawing.Size(279, 259)
+        Me.Question2.TabIndex = 8
         '
-        'RadioButton4
+        'Question2Text
         '
-        Me.RadioButton4.AutoSize = True
-        Me.RadioButton4.Location = New System.Drawing.Point(3, 124)
-        Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(93, 17)
-        Me.RadioButton4.TabIndex = 6
-        Me.RadioButton4.TabStop = True
-        Me.RadioButton4.Text = "a primary color"
-        Me.RadioButton4.UseVisualStyleBackColor = True
+        Me.Question2Text.Location = New System.Drawing.Point(6, 55)
+        Me.Question2Text.Name = "Question2Text"
+        Me.Question2Text.Size = New System.Drawing.Size(100, 20)
+        Me.Question2Text.TabIndex = 6
         '
-        'QuizNext5
+        'Question2Submit
         '
-        Me.QuizNext5.Location = New System.Drawing.Point(3, 154)
-        Me.QuizNext5.Name = "QuizNext5"
-        Me.QuizNext5.Size = New System.Drawing.Size(75, 23)
-        Me.QuizNext5.TabIndex = 5
-        Me.QuizNext5.Text = "Next"
-        Me.QuizNext5.UseVisualStyleBackColor = True
+        Me.Question2Submit.Location = New System.Drawing.Point(7, 95)
+        Me.Question2Submit.Name = "Question2Submit"
+        Me.Question2Submit.Size = New System.Drawing.Size(75, 23)
+        Me.Question2Submit.TabIndex = 5
+        Me.Question2Submit.Text = "Next"
+        Me.Question2Submit.UseVisualStyleBackColor = True
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Location = New System.Drawing.Point(3, 28)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(203, 13)
+        Me.Label8.Size = New System.Drawing.Size(218, 13)
         Me.Label8.TabIndex = 1
-        Me.Label8.Text = "According to the reading, which is green?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
-        'CapBa
-        '
-        Me.CapBa.AutoSize = True
-        Me.CapBa.Location = New System.Drawing.Point(3, 101)
-        Me.CapBa.Name = "CapBa"
-        Me.CapBa.Size = New System.Drawing.Size(50, 17)
-        Me.CapBa.TabIndex = 3
-        Me.CapBa.TabStop = True
-        Me.CapBa.Text = "a Bat"
-        Me.CapBa.UseVisualStyleBackColor = True
-        '
-        'CapB
-        '
-        Me.CapB.AccessibleName = ""
-        Me.CapB.AutoSize = True
-        Me.CapB.Location = New System.Drawing.Point(3, 78)
-        Me.CapB.Name = "CapB"
-        Me.CapB.Size = New System.Drawing.Size(55, 17)
-        Me.CapB.TabIndex = 4
-        Me.CapB.TabStop = True
-        Me.CapB.Text = "a Frog"
-        Me.CapB.UseVisualStyleBackColor = True
-        '
-        'RadioButton9
-        '
-        Me.RadioButton9.AutoSize = True
-        Me.RadioButton9.Location = New System.Drawing.Point(3, 55)
-        Me.RadioButton9.Name = "RadioButton9"
-        Me.RadioButton9.Size = New System.Drawing.Size(86, 17)
-        Me.RadioButton9.TabIndex = 2
-        Me.RadioButton9.TabStop = True
-        Me.RadioButton9.Text = "A mushroom " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        Me.RadioButton9.UseVisualStyleBackColor = True
+        Me.Label8.Text = "What was the name of the town Phillip lives?"
         '
         'QuizNext4
         '
@@ -519,32 +476,32 @@ Partial Class Demo_Quizes
         Me.ClientSize = New System.Drawing.Size(875, 489)
         Me.Controls.Add(Me.qu1q2)
         Me.Controls.Add(Me.Qu1q4)
-        Me.Controls.Add(Me.qu1q5)
+        Me.Controls.Add(Me.Question2)
         Me.Controls.Add(Me.qu1q3)
-        Me.Controls.Add(Me.qu1q)
+        Me.Controls.Add(Me.Question1)
         Me.Name = "Demo_Quizes"
         Me.Text = "Demo_Quizes"
-        Me.qu1q.ResumeLayout(False)
-        Me.qu1q.PerformLayout()
+        Me.Question1.ResumeLayout(False)
+        Me.Question1.PerformLayout()
         Me.qu1q3.ResumeLayout(False)
         Me.qu1q3.PerformLayout()
         Me.qu1q2.ResumeLayout(False)
         Me.qu1q2.PerformLayout()
         Me.Qu1q4.ResumeLayout(False)
         Me.Qu1q4.PerformLayout()
-        Me.qu1q5.ResumeLayout(False)
-        Me.qu1q5.PerformLayout()
+        Me.Question2.ResumeLayout(False)
+        Me.Question2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents NewYork As RadioButton
-    Friend WithEvents Sydney As RadioButton
-    Friend WithEvents Chicago As RadioButton
-    Friend WithEvents QuizNext1 As Button
-    Friend WithEvents qu1q As Panel
+    Friend WithEvents Answer1a As RadioButton
+    Friend WithEvents Answer1c As RadioButton
+    Friend WithEvents Answer1b As RadioButton
+    Friend WithEvents Question1Submit As Button
+    Friend WithEvents Question1 As Panel
     Friend WithEvents qu1q3 As Panel
     Friend WithEvents QuizNext3 As Button
     Friend WithEvents Label4 As Label
@@ -557,12 +514,9 @@ Partial Class Demo_Quizes
     Friend WithEvents Canberrar As RadioButton
     Friend WithEvents Canberramen As RadioButton
     Friend WithEvents Canberrite As RadioButton
-    Friend WithEvents qu1q5 As Panel
-    Friend WithEvents QuizNext5 As Button
+    Friend WithEvents Question2 As Panel
+    Friend WithEvents Question2Submit As Button
     Friend WithEvents Label8 As Label
-    Friend WithEvents CapBa As RadioButton
-    Friend WithEvents CapB As RadioButton
-    Friend WithEvents RadioButton9 As RadioButton
     Friend WithEvents QuizNext4 As Button
     Friend WithEvents Label10 As Label
     Friend WithEvents CapA As RadioButton
@@ -574,9 +528,9 @@ Partial Class Demo_Quizes
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents F As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Answer1d As RadioButton
     Friend WithEvents Turcor As RadioButton
     Friend WithEvents Canberran As RadioButton
-    Friend WithEvents RadioButton4 As RadioButton
     Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents Question2Text As TextBox
 End Class
